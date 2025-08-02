19049007de4695218a1388f45fd41ccda61dea44
pranav perla <132197230+pranavperla@users.noreply.github.com>
2025-08-02 13:04:26 +0000

    Update workflow to apply Terraform and push Docker image to ECR

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "ec2demo" {
+  name                 = "ec2demo"
+  image_tag_mutability = "MUTABLE"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
