7ce9ffb44f4150e742e3417a8a72b7331276c255
pranav perla <132197230+pranavperla@users.noreply.github.com>
2025-08-02 19:48:10 +0530

    include the new resource for ecr registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,21 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_ecr_repository" "ec2demo" {
-  name                 = "ec2demo"
+resource "aws_ecr_repository" "ec2demo-frontend" {
+  name                 = "ec2demo-frontend"
+  image_tag_mutability = "MUTABLE"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+resource "aws_ecr_repository" "ec2demo-backend" {
+  name                 = "ec2demo-backend"
   image_tag_mutability = "MUTABLE"
 
   lifecycle {
